ba0d3008b56fc5e6216322916eeaeb631e0cd6d7
Nishi Parameshwara <niship007@gmail.com>
2024-02-12 02:26:25 -0800

    Added four functionalities

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Features and learnings:
 
 1. Use tool directly from command line.
 2. Use tool without any reference to python.
-3. Build, distribute and licensing.
-4. Use valid libraries and tools.
+3. Error Handling.
+4. Build, distribute and licensing.
--- wc.py
+++ wc.py
@@ -1,4 +1,58 @@
 import sys
+import os
 
-for arg in sys.argv:
-  print(arg)
+# Print out the number of bytes in a file
+def numberOfBytes(filename):
+  if sys.argv[1] == "-c":
+    print(os.path.getsize(filename), end = " ")
+    print(filename)
+  
+# Print out the number of lines in a file
+def numberOfLines(filename):
+  if sys.argv[1] == "-l":
+    with open(filename, "r", encoding = "utf8") as f:
+      # f.readlines() reads the file in a single go and returns the lines of the files as list string elements
+      print(len(f.readlines()), end = " ") 
+    print(filename)
+  
+# Print out the number of words in a file
+def numberOfWords(filename):
+  if sys.argv[1] == "-w":
+    with open(filename, "r", encoding = "utf8") as f:
+      fileContent = f.read()
+      wordList = fileContent.split()
+      wordCount = len(wordList)
+      print(wordCount, end = " ")
+    print(filename)
+  
+# Print out the number of characters in a file
+def numberOfCharacters(filename):
+  if sys.argv[1] == "-m":
+    with open(filename, "r", encoding = "utf8") as f:
+      fileContentWordList = f.read().strip().split()
+      charLen = sum(len(word) for word in fileContentWordList)
+      print(charLen, end = " ")
+    print(filename)
+    
+
+# Read from standard input if filename is not provided
+
+def main():
+  if sys.argv[1] not in ["-c", "-l", "-w", "-m"]:
+    numberOfBytes(filename)
+    numberOfLines(filename)
+    numberOfWords(filename)
+    numberOfCharacters(filename)
+  else:
+      filename = sys.argv[2]
+      if sys.argv[1] == "-c":
+        numberOfBytes(filename)
+      elif sys.argv[1] == "-l":
+        numberOfLines(filename)
+      elif sys.argv[1] == "-w":
+        numberOfWords(filename)
+      elif sys.argv[1] == "-m":
+        numberOfCharacters(filename)
+        
+if __name__ == "__main__":
+  main()
